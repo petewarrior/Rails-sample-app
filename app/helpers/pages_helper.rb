b9a75fa4b9502cb95126e38a45aa281078597edf
Arya Antaputra <arya@petewarrior.com>
2011-11-25 01:04:51 +0700

    tried out helper

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,13 @@
 module PagesHelper
+    
+  def title
+    title_base = "Ruby test app"
+    
+    if @title.nil?
+      title_base
+    else
+      "#{title_base} | #{@title}"
+    end
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ RailsSampleApp::Application.routes.draw do
   get "pages/contact"
   
   get "pages/about"
+  
+  get "pages/goat"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
